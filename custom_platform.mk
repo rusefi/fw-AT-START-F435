ba4e2b4ac43abd63098045e2c5ed80df822b7318
rusefillc <sdfsdfqsf2334234234>
2023-05-23 18:53:48 -0400

    progress

--- custom_platform.mk
+++ custom_platform.mk
@@ -1,3 +1,3 @@
 	CPU_STARTUP_DIR = $(ARTERY_CONTRIB)/os/common/startup/ARMCMx/compilers/GCC/mk/startup_at32f435.mk
 	CPU_PLATFORM = $(ARTERY_CONTRIB)/os/hal/ports/ARTERY/AT32F435.mk
-	CPU_HWLAYER = ports/artery
+	CPU_HWLAYER = ../../../../hw_layer_at
---
+++ hw_layer_at/hw_ports.mk
@@ -0,0 +1,1 @@
+MCU = cortex-m4
