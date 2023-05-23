f9233bc3cb6deaeed2a7b94e5d36be0e8061dbec
rusefillc <sdfsdfqsf2334234234>
2023-05-23 17:31:21 -0400

    progress

---
+++ compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# todo: remove 'atlas' part once we have https://github.com/rusefi/rusefi/issues/5284
+cd ext/rusefi/firmware/config/boards/atlas
+
+bash ../common_make.sh ../../../../.. custom_platform
