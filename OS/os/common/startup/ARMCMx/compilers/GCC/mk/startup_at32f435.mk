fab13dd5d2bb69ff2ed6876d698f2b9981f1e9b1
rusefillc <sdfsdfqsf2334234234>
2023-05-23 18:22:48 -0400

    progress

---
+++ OS/os/common/startup/ARMCMx/compilers/GCC/mk/startup_at32f435.mk
@@ -0,0 +1,25 @@
+# shameless copy-paste of startup_ke1xf.mk
+
+
+
+
+
+STARTUPSRC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt1.c
+
+STARTUPASM = $(ARTERY_CONTRIB)/os/common/startup/ARMCMx/compilers/GCC/startup_at32f435.S \
+             $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt0_v7m.S \
+             $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/vectors.S
+
+STARTUPINC = $(ARTERY_CONTRIB)/os/common/startup/ARMCMx/devices/AT32F435 \
+             $(ARTERY_CONTRIB)/os/common/ext/CMSIS/ARTERY \
+             $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC \
+             $(CHIBIOS)/os/common/ext/CMSIS/include \
+             $(CHIBIOS)/os/common/ext/ARM/CMSIS/Core/Include
+
+STARTUPLD_KINETIS  = $(KINETIS_CONTRIB)/os/common/startup/ARMCMx/compilers/GCC/ld
+STARTUPLD = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/ld
+
+# Shared variables
+ALLXASMSRC += $(STARTUPASM)
+ALLCSRC    += $(STARTUPSRC)
+ALLINC     += $(STARTUPINC)
---
+++ custom_platform.mk
@@ -0,0 +1,3 @@
+	CPU_STARTUP_DIR = $(ARTERY_CONTRIB)/os/common/startup/ARMCMx/compilers/GCC/mk/startup_at32f435.mk
+	CPU_PLATFORM = $(ARTERY_CONTRIB)/os/hal/ports/ARTERY/AT32F435.mk
+	CPU_HWLAYER = ports/artery
