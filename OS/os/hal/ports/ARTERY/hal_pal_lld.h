b57022b2238a9b75adde9eddd47d013f901545e0
rusefillc <sdfsdfqsf2334234234>
2023-05-23 22:51:28 -0400

    driver progress

---
+++ OS/os/hal/ports/ARTERY/hal_pal_lld.h
@@ -0,0 +1,38 @@
+/**
+ * @brief   Digital I/O port sized unsigned type.
+ */
+typedef uint32_t ioportmask_t;
+
+/**
+ * @brief   Digital I/O modes.
+ */
+typedef uint32_t iomode_t;
+
+/**
+ * @brief   Type of an I/O line.
+ */
+typedef uint32_t ioline_t;
+
+/**
+ * @brief   Type of an event mode.
+ */
+typedef uint32_t ioeventmode_t;
+
+typedef gpio_type *ioportid_t;
+
+/**
+ * @brief   Type of an pad identifier.
+ */
+typedef uint32_t iopadid_t;
+
+#define pal_lld_init(config) _pal_lld_init(config)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  void _pal_lld_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
--- halconf.h
+++ halconf.h
@@ -100,7 +100,7 @@
  * @brief   Enables the SPI subsystem.
  */
 #if !defined(HAL_USE_SPI) || defined(__DOXYGEN__)
-#define HAL_USE_SPI                 TRUE
+#define HAL_USE_SPI                 FALSE
 #endif
 
 /**
