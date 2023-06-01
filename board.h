5c51399a236739c1d41271463dcd7311b46f2878
Andrey <rusefillc@gmail.com>
2023-05-31 20:03:47 -0400

    wow progress

--- board.h
+++ board.h
@@ -10,4 +10,22 @@
 #define LED_CRITICAL_ERROR_BRAIN_PIN Gpio::Unassigned
 #endif
 
+/*
+ * Board oscillators-related settings.
+ * NOTE: LSE not fitted.
+ */
+#if !defined(STM32_LSECLK)
+#define STM32_LSECLK                0U
+#endif
+
+#if !defined(STM32_HSECLK)
+#define STM32_HSECLK                8000000U
+#endif
+
+/*
+ * Board voltages.
+ * Required for performance limits calculation.
+ */
+#define STM32_VDD                   300U
+
 #endif /* _BOARD_H_ */
